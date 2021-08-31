51282e7d94e58186bbd2e052bf4dcbaa83fab719
sky6min <sky6min@gmail.com>
2021-08-31 11:07:54 +0900

    Initial commit

---
+++ src/main/java/mrPang/lolnews/LolnewsApplication.java
@@ -0,0 +1,13 @@
+package mrPang.lolnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mrPang/lolnews/LolnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package mrPang.lolnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
